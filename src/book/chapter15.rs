b9431de56a8cf256ecfe1eba1c302f1f1cc1e37f
Camille Vauchel <camille.vauchel@gmail.com>
2024-08-01 22:15:54 +0200

    Start deref

--- src/book/chapter15.rs
+++ src/book/chapter15.rs
@@ -1,13 +1,13 @@
-use std::usize;
+use std::{ops::Deref, usize};
 
 #[allow(dead_code)]
 pub fn execute_chapter15() {
-    create_cons_list();
+    execute_my_box();
 }
 
 // A kind of smart pointer
 #[allow(dead_code)]
-fn create_box() {
+fn execute_box() {
     let b = Box::new(1);
     println!("{}", b);
 }
@@ -27,7 +27,8 @@ impl<T> List<T> {
     }
 }
 
-fn create_cons_list() {
+#[allow(dead_code)]
+fn execute_cons_list() {
     let l2 = List::Cons(5, Box::new(List::Nil));
     let l3 = List::Cons("a", Box::new(List::Cons("b", Box::new(List::Nil))));
 
@@ -35,4 +36,36 @@ fn create_cons_list() {
     println!("{:?}", l3);
 
     println!("{}", l3.size());
+}
+
+#[allow(dead_code)]
+fn execute_deref() {
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> Self {
+        Self(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[allow(dead_code)]
+fn execute_my_box() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, *y);
 }
